a717a89fe1714b57a97f06c0e9730d6a1ccf411f
LeeHareum <g029843507@gmail.com>
2024-07-05 13:42:14 +0900

    add:무한스크롤,로딩스피너

--- src/app/api/pokemons/route.ts
+++ src/app/api/pokemons/route.ts
@@ -1,33 +1,45 @@
+// src/app/api/pokemons/route.ts
 import { NextResponse } from "next/server";
 import axios from "axios";
 
 const TOTAL_POKEMON = 151;
+const PAGE_SIZE = 20;
+
+const isNotNull = <T>(value: T | null): value is T => value !== null;
 
 export const GET = async (request: Request) => {
+  const { searchParams } = new URL(request.url);
+  const page = parseInt(searchParams.get("page") || "1");
+  const offset = (page - 1) * PAGE_SIZE;
+
   try {
-    const allPokemonPromises = Array.from(
-      { length: TOTAL_POKEMON },
-      (_, index) => {
-        const id = index + 1;
-        return Promise.all([
-          axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
-          axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
-        ]);
-      }
-    );
+    const pokemonPromises = Array.from({ length: PAGE_SIZE }, (_, index) => {
+      const id = index + 1 + offset;
+      if (id > TOTAL_POKEMON) return null;
 
-    const allPokemonResponses = await Promise.all(allPokemonPromises);
+      return Promise.all([
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
+        axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
+      ]);
+    }).filter(isNotNull);
 
-    const allPokemonData = allPokemonResponses.map(
-      ([response, speciesResponse], index) => {
-        const koreanName = speciesResponse.data.names.find(
-          (name: any) => name.language.name === "ko"
-        );
-        return { ...response.data, korean_name: koreanName?.name || null };
-      }
-    );
+    const pokemonResponses = await Promise.all(pokemonPromises);
 
-    return NextResponse.json(allPokemonData);
+    const pokemonData = pokemonResponses.map(([response, speciesResponse]) => {
+      const koreanName = speciesResponse.data.names.find(
+        (name: any) => name.language.name === "ko"
+      );
+      return { ...response.data, korean_name: koreanName?.name || null };
+    });
+
+    const totalPages = Math.ceil(TOTAL_POKEMON / PAGE_SIZE);
+    const hasNextPage = page < totalPages;
+
+    return NextResponse.json({
+      data: pokemonData,
+      totalPages,
+      hasNextPage,
+    });
   } catch (error) {
     return NextResponse.json({ error: "Failed to fetch data" });
   }
--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -2,31 +2,58 @@
 
 import axios from "axios";
 import { Pokemon } from "@/types/pokemon";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PokemonList = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [loading, setLoading] = useState(true);
   const [err, setErr] = useState<string | null>(null);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(true);
+
+  const observer = useRef<IntersectionObserver | null>(null);
+  const lastPokemonElementRef = useRef<HTMLDivElement | null>(null);
+
+  const fetchPokemons = async (page: number) => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`/api/pokemons?page=${page}`);
+      const newPokemons = response.data.data;
+      setPokemons((prevPokemons) => [...prevPokemons, ...newPokemons]);
+      setHasNextPage(response.data.hasNextPage);
+    } catch (error) {
+      setErr("데이터 가져오기에 실패했습니다.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchPokemons = async () => {
-      try {
-        const response = await axios.get("/api/pokemons");
-        setPokemons(response.data);
-      } catch (error) {
-        setErr("데이터 가져오기에 실패했습니다.");
-      } finally {
-        setLoading(false);
+    fetchPokemons(page); // 페이지 변경 시 데이터 가져오기
+  }, [page]);
+
+  useEffect(() => {
+    if (loading) return; // 로딩 중일 경우 반환
+    if (observer.current) observer.current.disconnect(); // 기존 관찰자 연결 해제
+
+    const callback = (entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting && hasNextPage) {
+        setPage((prevPage) => prevPage + 1); // 마지막 요소가 보일 경우 페이지 증가
       }
     };
-    fetchPokemons();
-  }, []);
 
-  if (loading) return <div>로딩중..</div>;
+    observer.current = new IntersectionObserver(callback); // 새로운 관찰자 생성
+    if (lastPokemonElementRef.current) {
+      observer.current.observe(lastPokemonElementRef.current); // 마지막 요소 관찰 시작
+    }
+  }, [loading, hasNextPage]);
+
+  if (loading && pokemons.length === 0) return <LoadingSpinner />;
   if (err) return <div>{err}</div>;
+  if (pokemons.length === 0) return <div>포켓몬 데이터가 없습니다.</div>;
 
   return (
     <div className="p-10 bg-gray-800 min-h-screen">
@@ -34,32 +61,45 @@ const PokemonList = () => {
         포켓몬 도감
       </p>
       <ul className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 text-white">
-        {pokemons.map((pokemon) => (
-          <li
-            className="bg-gray-900 border-solid border-2 rounded-lg border-gray-700 p-4 sm:p-7 flex flex-col items-center"
-            key={pokemon.id}
-          >
-            <Link href={`/pokemon/${pokemon.id}`}>
-              <div className="cursor-pointer max-w-2xl mx-auto transform transition duration-500 hover:scale-105">
-                <Image
-                  className="sm:w-30 sm:h-30 p-3 sm:p-5 bg-white border-solid border rounded-lg border-black mb-2 animate-glow"
-                  src={pokemon.sprites.front_default}
-                  alt={pokemon.name}
-                  priority
-                  width={180}
-                  height={180}
-                />
-                <p className="text-xs mt-3 mb-1 text-center">
-                  No . {pokemon.id}
-                </p>
-                <p className="text-base sm:text-lg text-center">
-                  {pokemon.korean_name}
-                </p>
-              </div>
-            </Link>
-          </li>
-        ))}
+        {pokemons &&
+          Array.isArray(pokemons) &&
+          pokemons.map((pokemon, index) => (
+            <li
+              key={`${pokemon.id}-${index}`}
+              className="bg-gray-900 border-solid border-2 rounded-lg border-gray-700 p-4 sm:p-7 flex flex-col items-center"
+            >
+              <Link href={`/pokemon/${pokemon.id}`}>
+                <div
+                  className="cursor-pointer max-w-2xl mx-auto transform transition duration-500 hover:scale-105"
+                  ref={
+                    index === pokemons.length - 1 ? lastPokemonElementRef : null // 마지막 포켓몬 요소 참조 추가
+                  }
+                >
+                  <Image
+                    className="sm:w-30 sm:h-30 p-3 sm:p-5 bg-white border-solid border rounded-lg border-black mb-2 animate-glow"
+                    src={pokemon.sprites.front_default}
+                    alt={pokemon.name}
+                    priority
+                    width={180}
+                    height={180}
+                  />
+                  <p className="text-xs mt-3 mb-1 text-center">
+                    No . {pokemon.id}
+                  </p>
+                  <p className="text-base sm:text-lg text-center">
+                    {pokemon.korean_name}
+                  </p>
+                </div>
+              </Link>
+            </li>
+          ))}
       </ul>
+      {loading && <div className="mt-10 text-center text-white">로딩중...</div>}
+      {!hasNextPage && (
+        <div className="mt-10 text-center text-white">
+          더 이상 불러올 포켓몬이 없습니다.
+        </div>
+      )}
     </div>
   );
 };
