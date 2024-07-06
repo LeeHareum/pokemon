228ea1e66e3892b37de43ee0f909f7f66d90e72a
LeeHareum <g029843507@gmail.com>
2024-07-07 03:57:19 +0900

    style:spinner위치수정

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -8,7 +8,7 @@ export const LoadingSpinnerBlack = () => {
 
 export const LoadingSpinnerWhite = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex mt-9 items-top justify-center min-h-screen">
       <div className="w-16 h-16 border-4 border-white border-dashed rounded-full animate-spin"></div>
     </div>
   );
