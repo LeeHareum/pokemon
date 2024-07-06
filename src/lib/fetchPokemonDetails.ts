b02df3705998ec071ad130df733119f5dbed8c56
LeeHareum <g029843507@gmail.com>
2024-07-06 23:38:59 +0900

    수정

--- src/lib/fetchPokemonDetails.ts
+++ src/lib/fetchPokemonDetails.ts
@@ -1,17 +1,30 @@
 import axios from "axios";
 import { Pokemon } from "@/types/pokemon";
 
-export const fetchPokemonDetails = async (
-  id: string
-): Promise<Pokemon | null> => {
+// export const fetchPokemonDetails = async (
+//   id: string
+// ): Promise<Pokemon | null> => {
+//   try {
+//     const response = await axios.get(
+//       `https://pokemon-five-gray.vercel.app/pokemon/${id}` // 배포링크
+//     );
+//     console.log(response.data);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching pokemon details:", error);
+//     return null;
+//   }
+// };
+
+export const fetchPokemonDetail = async (id: string) => {
   try {
-    const response = await axios.get(
-      `https://pokemon-five-gray.vercel.app/pokemon/${id}` // 배포링크
+    const response = await fetch(
+      `https://pokemon-five-gray.vercel.app/pokemon/${id}`
     );
-    console.log(response.data);
-    return response.data;
+    const pokemon = await response.json();
+    return pokemon;
   } catch (error) {
-    console.error("Error fetching pokemon details:", error);
+    console.error(error);
     return null;
   }
 };
